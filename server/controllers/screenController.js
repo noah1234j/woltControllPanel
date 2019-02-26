157791766bb5e0d7bd1283e371be6ef0da9faec8
Noah Jorgensen <noaharkjorgensen@gmail.com>
2019-02-26 11:30:56 -0600

    Update screenController.js

--- server/controllers/screenController.js
+++ server/controllers/screenController.js
@@ -1,49 +1,49 @@
-let Scrn = require('../models/screen/index')
-let scrn = new Scrn()
+// let Scrn = require('../models/screen/index')
+// let scrn = new Scrn()
 
-async function scrnHandler (req, res) {
+// async function scrnHandler (req, res) {
 
-    req.body.endPoint = "/api/screen/"
+//     req.body.endPoint = "/api/screen/"
 
-    switch (req.body.command) {
-        case "up": 
-            if (await scrn.up() == "MOVING UP") {
-                successHandler(req, res, "Success: The projector is successfully moving up")
-            } else {
-                failHandler(req, res, "Error: The screen cannot move up")
-            }
-            break
-        case "down": 
-            if (await scrn.down() == "MOVING DOWN") {
-                successHandler(req, res, "Success: The screen is successfully moving down")
-            } else {
-                failHandler(req, res, "Error: The screen cannot move down")
-            }
-            break
-        case "stop": 
-            if (await scrn.stop() == "MANUALLY STOPPED") {
-                successHandler(req, res, "Success: The projector has successfully stopped")
-            } else {
-                failHandler(req, res, "Error: The screen cannot stop")
-            }
-            break
-        case "status":
-            successHandler(req, res, `The status of the projector is ${await scrn.status()}`)
-            break
-        default:
-            failHandler(req, res, "Error: Unknown Command, try up, down, status, stop")
-    }
-}
+//     switch (req.body.command) {
+//         case "up": 
+//             if (await scrn.up() == "MOVING UP") {
+//                 successHandler(req, res, "Success: The projector is successfully moving up")
+//             } else {
+//                 failHandler(req, res, "Error: The screen cannot move up")
+//             }
+//             break
+//         case "down": 
+//             if (await scrn.down() == "MOVING DOWN") {
+//                 successHandler(req, res, "Success: The screen is successfully moving down")
+//             } else {
+//                 failHandler(req, res, "Error: The screen cannot move down")
+//             }
+//             break
+//         case "stop": 
+//             if (await scrn.stop() == "MANUALLY STOPPED") {
+//                 successHandler(req, res, "Success: The projector has successfully stopped")
+//             } else {
+//                 failHandler(req, res, "Error: The screen cannot stop")
+//             }
+//             break
+//         case "status":
+//             successHandler(req, res, `The status of the projector is ${await scrn.status()}`)
+//             break
+//         default:
+//             failHandler(req, res, "Error: Unknown Command, try up, down, status, stop")
+//     }
+// }
 
-function successHandler(req, res, msg) {
-    req.body.code = 200;
-    req.body.message = msg;
-    res.send ( req.body );
-}
+// function successHandler(req, res, msg) {
+//     req.body.code = 200;
+//     req.body.message = msg;
+//     res.send ( req.body );
+// }
 
-function failHandler(req, res, msg) {
-    req.body.code = 400;
-    req.body.message = msg;
-    res.send( req.body );
-}
-module.exports = scrnHandler
+// function failHandler(req, res, msg) {
+//     req.body.code = 400;
+//     req.body.message = msg;
+//     res.send( req.body );
+// }
+// module.exports = scrnHandler
